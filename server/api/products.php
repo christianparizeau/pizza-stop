6c58d5e278e9579cb788f0e0c5d56b5c13d41496
Doctorbingbing <christianparizeau@gmail.com>
2019-11-18 21:14:08 +0000

    removed dummy message in response and filled it with product information from the mySQL server

--- server/api/products.php
+++ server/api/products.php
@@ -1,7 +1,16 @@
 <?php
 
-$response['body'] = [
-  'message' => 'Wow you actually did it?'
-];
+if ($request['method'] === 'GET') {
+  $link = get_db_link();
+  $all_product_info_except_longDescription = "SELECT productId,name, price, image, shortDescription FROM products";
+  $result = mysqli_query($link, $all_product_info_except_longDescription);
+  $message = [];
+  if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+      array_push($message, $row);
+    }
+  }
+  $response['body'] = $message;
+}
 
 send($response);
