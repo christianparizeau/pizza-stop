652fe31187f85c1717e77bf25f83c709f9f9027b
Doctorbingbing <christianparizeau@gmail.com>
2019-11-19 18:40:30 +0000

    when productId is specified in the query parameters of products api end point, now returns full description of the product

--- server/api/products.php
+++ server/api/products.php
@@ -1,9 +1,24 @@
 <?php
 
+
+if($request['query']['productId']){
+
+}
+
+
 if ($request['method'] === 'GET') {
   $link = get_db_link();
-  $all_product_info_except_longDescription = "SELECT productId,name, price, image, shortDescription FROM products";
-  $result = mysqli_query($link, $all_product_info_except_longDescription);
+  $productId = $request['query']['productId'];
+  if(isset($productId)){
+    if(is_numeric($productId) && intval($productId) != 0 ){
+    $query = "SELECT * FROM products WHERE productId = {$productId}";
+    } else{
+      throw new ApiError('productId must be a non-zero integer to be considered valid', 400);
+    }
+} else{
+  $query = "SELECT productId,name,price,image,shortDescription FROM products";
+}
+  $result = mysqli_query($link, $query);
   $message = [];
   if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
