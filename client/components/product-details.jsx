1c6048528fe9a498590e45e71c87fe971d92b7a5
Doctorbingbing <christianparizeau@gmail.com>
2019-11-19 21:16:08 +0000

    fixed display price on the product-description component

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -24,6 +24,7 @@ export default class ProductDetails extends React.Component {
   render() {
     if (!this.state.product) return null;
     const { name, price, image, shortDescription, longDescription } = this.state.product;
+    const displayPrice = '$' + (price / 100).toFixed(2);
     return (
       <div className='container'>
         <p className="text-muted" onClick={this.getCatalog}>{'< '}Back to catalog </p>
@@ -31,7 +32,7 @@ export default class ProductDetails extends React.Component {
           <img src={image} className="fixed-image-height" />
           <div>
             <h1>{name}</h1>
-            <p className="text-muted">{price}</p>
+            <p className="text-muted">{displayPrice}</p>
             <p>{shortDescription}</p>
           </div>
         </div>
