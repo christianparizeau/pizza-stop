ffbb453d61b7b1e5e8bc69ad5f3a9c7303c7a91b
Doctorbingbing <christianparizeau@gmail.com>
2020-01-23 18:47:18 +0000

    moved images from top of div in catalog view

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -9,7 +9,7 @@ export default function ProductListItem({ product, setView }) {
     <div className="card mb-4 pointer"
       onClick={viewSetter}
       style={{ width: '20rem' }}>
-      <img src={product.image} className="card-img-top fixed-image-height"></img>
+      <img src={product.image} className="card-img-top fixed-image-height mt-2"></img>
       <div className="card-body">
         <h5 className="card-title">{product.name}</h5>
         <h6 className='text-muted'>{price}</h6>
