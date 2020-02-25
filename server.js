8282a978847707a9162a152865c869d3a723c4b0
Doctorbingbing <christianparizeau@gmail.com>
2020-02-25 23:19:08 +0000

    created basic routing in express to deal with api requests

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const port = 9000;
 
-app.use(express.static('server/public'));
-app.get('/', (req, res) => res.send('Hello World'));
+router.use((req, res, next) => {
+  // eslint-disable-next-line
+  console.log('A response has been received!');
+  next();
+});
+router.get('/products', (req, res) => {
+  res.json({ message: 'Products worked!' });
+});
+router.get('/cart', (req, res) => {
+  res.json({ message: 'Cart worked!' });
+});
+router.get('/orders', (req, res) => {
+  res.json({ message: 'Orders worked!' });
+});
+
+app.use('/api', router);
+// app.get('/', (req, res) => res.send('Hello World'));
+// app.get('api/', (req, res) => res.send('This Worked!'));
 // eslint-disable-next-line
 app.listen(port, () => console.log(`Magic happens on port ${port}!`));
