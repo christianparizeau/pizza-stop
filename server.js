c64b25ea1cae94337332ae0123ab403da02ccccf
Christian <christianparizeau@gmail.com>
2020-02-26 10:37:51 -0800

    added mysql database connection to node server

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const port = 9000;
+const dbParams = require('./_config.js');
+const mysql = require('mysql');
+const host = dbParams.host;
+const user = dbParams.user;
+const password = dbParams.password;
+const database = dbParams.database;
+
+const connection = mysql.createConnection({ host, password, user, database });
+connection.connect();
 
 router.use((req, res, next) => {
   // eslint-disable-next-line
@@ -9,12 +18,15 @@ router.use((req, res, next) => {
   next();
 });
 router.get('/products', (req, res) => {
+  connection.end();
   res.json({ message: 'Products worked!' });
 });
 router.get('/cart', (req, res) => {
+  connection.end();
   res.json({ message: 'Cart worked!' });
 });
 router.get('/orders', (req, res) => {
+  connection.end();
   res.json({ message: 'Orders worked!' });
 });
 
